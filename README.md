26a70d29ace7473dcd2bd3af2d1411be1435fc9c
Dranyk <dvdranyk@gmail.com>
2023-03-26 15:58:13 +0300

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+https://ibb.co/NnLM61C
+# Отримуємо контакт по id
+https://ibb.co/vPxSwnp
+# Додаємо контакт
+https://ibb.co/6Nkkv8S
+# Видаляємо контакт
+https://ibb.co/dmZHpQk
--- contacts.js
+++ contacts.js
@@ -1,8 +1,7 @@
 const fs = require("fs").promises;
 const path = require("path");
-const contactsPath = path.join(__dirname, "db", "contacts.json");
 const { nanoid } = require("nanoid");
-
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 async function listContacts() {
   try {
     const data = await fs.readFile(contactsPath, "utf8");
